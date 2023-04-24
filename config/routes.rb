dd4d0e3bc5a440af771eecd04084c2c444893bd4
Sam Wells <sfwells0518@gmail.com>
2023-04-24 12:42:14 -0500

    add articles show REST route, show action in articles controller and show.html.erb view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "articles#index
-  
-  get "/articles", to: "articles#index"
+  root "articles#index"
+
+  get "/articles" => "articles#index"
+  get "/articles/:id" => "articles#show"
 end
